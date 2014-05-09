c2c87df41d6ca35c31b4e9e1b348a435f223b1dd
Kevin Yap <me@kevinyap.ca>
2014-05-09 16:15:10 -0700

    Added docstrings and better index handling

--- LeagueSalesBot.py
+++ LeagueSalesBot.py
@@ -5,7 +5,7 @@ import os, sys, re, datetime, time
 import httplib2, praw
 import settings, lastrun
 
-# Create classes for sale types (skins and champions)
+"""Create classes for sale types (skins and champions)"""
 class Sale():
     saleName = ""
     regularCost = 0
@@ -22,7 +22,7 @@ class Champ(Sale):
     isSkin = False
     splashName = ""
 
-# Define variables for ANSI-coloured terminal messages
+"""Define variables for ANSI-coloured terminal messages"""
 kWarning = '\033[31m'
 kSuccess = '\033[32m'
 kSpecial = '\033[36m'
@@ -30,10 +30,8 @@ kReset = '\033[0m'
 
 def getContent(testLink = None):
     if testLink:
-        if "//na" in testLink:
-            naLink, euwLink = testLink, "/#"
-        else:
-            naLink, euwLink = "/#", testLink
+        naLink = testLink if "//na" in testLink else "/#"
+        euwLink = testLink if "//euw" in testLink else "/#"
 
         if not "-v" in sys.argv:
             print testLink + "...",
@@ -54,17 +52,14 @@ def getContent(testLink = None):
             saleEnd = datetime.datetime.strptime(end, "%m%d")
 
     else:
-        # Get string of end of last sale from lastrun.py
+        """
+        If lastrun.rotation is even, the last sale was posted on a Thursday so the next sale will start a day after the
+        end date of the previous sale. Otherwise, the next sale will start on the same day that the last sale ended on.
+        """
         lastSaleEnd = datetime.datetime.strptime(lastrun.lastSaleEnd, "%Y-%m-%d")
-
-        # If lastrun.rotation is even, the last sale was posted on a Thursday so the next sale will start a day after the
-        # end date of the previous sale. Otherwise, the next sale will start on the same day that the last sale ended on.
         saleStart = lastSaleEnd + datetime.timedelta((lastrun.rotation + 1) % 2)
+        saleEnd = saleStart + datetime.timedelta(3) # Four-day-long sales
 
-        # Sales always end 3 days after they start (four-day-long sales)
-        saleEnd = saleStart + datetime.timedelta(3)
-
-        # Insert leading hyphen to strip leading zero
         mdStart, mdEnd = saleStart.strftime("%m%d"), saleEnd.strftime("%m%d")
         dmStart, dmEnd = saleStart.strftime("%d%m"), saleEnd.strftime("%d%m")
 
@@ -80,7 +75,7 @@ def getContent(testLink = None):
         naLink, euwLink = None, None
 
         while (not naLink) and (not euwLink):
-            for link in links:
+            for i, link in enumerate(links):
                 try:
                     header, content = httplib2.Http().request(link)
                 except httplib2.ServerNotFoundError:
@@ -92,7 +87,6 @@ def getContent(testLink = None):
                     print kWarning + str(header.status) + kReset
                 else:
                     print kSuccess + "200" + kReset
-                    i = links.index(link)
                     naLink, euwLink = links[i % 2], links[i % 2 + 2]
                     break
             else:
@@ -112,6 +106,7 @@ def getContent(testLink = None):
     return content, dateRange, naLink, euwLink
 
 def saleOutput(sale):
+    """Generates one row of sale table"""
     if sale.isSkin:
         # Champions with multi-part names
         for key, value in settings.multiNames.iteritems():
@@ -148,6 +143,7 @@ def saleOutput(sale):
     return "|{0}|**[{1}]({2})**|{3} RP|~~{4} RP~~|{5}|".format(sale.icon, sale.saleName, sale.wikiLink, str(sale.saleCost), str(sale.regularCost), mediaString)
 
 def makePost(saleArray, dateRange, naLink, euwLink = "/#"):
+    """Formats sale data into Reddit post"""
     rotationSchedule = [[975, 750, 520], [1350, 975, 520], [975, 750, 520], [975, 975, 520]]
     nextRotation = rotationSchedule[lastrun.rotation % 4]
 
@@ -175,11 +171,15 @@ def makePost(saleArray, dateRange, naLink, euwLink = "/#"):
 
 
 def getSpotlight(name, isSkin):
+    """Finds appropriate champion or skin spotlight video for sale"""
     if isSkin:
-        url = "https://www.youtube.com/user/SkinSpotlights/search?query=" + name.replace(" ", "+") + "+skin+spotlight"
+        channel = "SkinSpotlights"
+        suffix = "+skin+spotlight"
     else:
-        url = "https://www.youtube.com/user/RiotGamesInc/search?query=" + name.replace(" ", "+") + "+champion+spotlight"
+        channel = "RiotGamesInc"
+        suffix = "+champion+spotlight"
 
+    url = "https://www.youtube.com/user/" + channel + "/search?query=" + name.replace(" ", "+") + suffix
     header, content = httplib2.Http().request(url)
 
     try:
@@ -189,42 +189,42 @@ def getSpotlight(name, isSkin):
     else:
         return "https://www.youtube.com" + slug, vidTitle
 
+def updateLastRun():
+    """Make appropriate changes to lastrun.py if post succeeds"""
+    saleEndText = (datetime.datetime.now() + datetime.timedelta(4)).strftime("%Y-%m-%d")
+    directory = os.path.dirname(os.path.abspath(__file__))
+    path = os.path.join(directory, 'lastrun.py')
+    with open(path, 'r+') as f:
+        f.write("lastSaleEnd = \"{0}\"\nrotation = {1}\n".format(saleEndText, str((lastrun.rotation + 5) % 4)))
+
+    print kSuccess + "Updated lastrun.py." + kReset
 
 def submitPost(postTitle, postBody):
+    """Post to subreddits defined in settings.py"""
     r = praw.Reddit(user_agent=settings.userAgent)
     r.login(settings.username, settings.password)
     for subreddit in settings.subreddits:
         r.submit(subreddit, postTitle, text=postBody)
+
     print kSuccess + "Post successfully submitted to " + ", ".join(settings.subreddits) + "." + kReset
 
-    # Make appropriate changes to lastrun.py if post succeeds
-    saleEndText = (datetime.datetime.now() + datetime.timedelta(4)).strftime("%Y-%m-%d")
 
-    directory = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(directory, 'lastrun.py')
-    f = open(path, 'r+')
-    f.write("lastSaleEnd = \"{0}\"\nrotation = {1}\n".format(saleEndText, str((lastrun.rotation + 5) % 4)))
-    f.close()
-
-    print kSuccess + "Updated lastrun.py." + kReset
-
-
-def parseData(content):
+def parseSales(content):
+    """Finds sale data from sale page"""
     saleRegex = re.compile("<h4>(?:<a href.+?>)*\s*?(.+?)\s*?(?:<\/a>)*<\/h4>\s+?<strike.*?>(\d{3,4})<\/strike> (\d{3,4}) RP")
     imageRegex = re.compile("(http://riot-web-static\.s3\.amazonaws\.com/images/news/Skin_Sales/\S*?\.jpg)")
     saleArray = [Skin(), Skin(), Skin(), Champ(), Champ(), Champ()]
 
-    for sale in saleArray:
-        i = saleArray.index(sale)
+    for i, sale in enumerate(saleArray):
         sale.saleName, sale.regularCost, sale.saleCost = re.findall(saleRegex, content)[i]
         sale.spotlight, sale.vidTitle = getSpotlight(sale.saleName, sale.isSkin)
 
-        if not "-v" in sys.argv: # Terminal output
+        if not "-v" in sys.argv:
             print "{0} ({1} RP), {2}".format(sale.saleName, sale.saleCost, sale.vidTitle or "No spotlight found")
 
         if sale.isSkin:
-            sale.splashArt = re.findall(imageRegex, content)[(i*4)]
-            sale.inGameArt = re.findall(imageRegex, content)[(i*4)+2]
+            sale.splashArt = re.findall(imageRegex, content)[i * 4]
+            sale.inGameArt = re.findall(imageRegex, content)[(i * 4) + 2]
 
     return saleArray
 
@@ -232,7 +232,7 @@ if __name__ == "__main__":
     testLink = next((sys.argv[i] for i, arg in enumerate(sys.argv) if "http://" in arg), None)
 
     content, dateRange, naLink, euwLink = getContent(testLink)
-    saleArray = parseData(content)
+    saleArray = parseSales(content)
     postTitle, postBody = makePost(saleArray, dateRange, naLink, euwLink)
 
     if "-v" in sys.argv:
@@ -242,5 +242,6 @@ if __name__ == "__main__":
 
     if not testLink:
         submitPost(postTitle, postBody)
+        updateLastRun()
 
     sys.exit(0)
