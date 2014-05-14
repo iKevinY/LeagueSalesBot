616559b7d67ce955c742233abfa212ebd51a9097
Kevin Yap <me@kevinyap.ca>
2014-05-13 18:19:33 -0700

    Substituted champion splash art for info pages

--- LeagueSalesBot.py
+++ LeagueSalesBot.py
@@ -15,12 +15,11 @@ class Sale():
     icon = ""
 class Skin(Sale):
     isSkin = True
-    skinName = ""
     splashArt = ""
     inGameArt = ""
 class Champ(Sale):
     isSkin = False
-    splashName = ""
+    infoPage = ""
 
 """Define variables for ANSI-coloured terminal messages"""
 kWarning = '\033[31m'
@@ -28,7 +27,7 @@ kSuccess = '\033[32m'
 kSpecial = '\033[36m'
 kReset = '\033[0m'
 
-def getRange(saleStart, saleEnd):
+def formatRange(saleStart, saleEnd):
     """Returns properly formatted date range for sale start and end"""
     if saleStart.month == saleEnd.month:
         return "{0}–{1}".format(saleStart.strftime("%B %-d"), saleEnd.strftime("%-d"))
@@ -48,7 +47,7 @@ def getContent(testLink, delay, refresh, verbose):
         try:
             header, content = httplib2.Http().request(testLink)
         except httplib2.ServerNotFoundError:
-            sys.exit(kWarning + "Connection error. " + kReset + "Terminating script.")
+            sys.exit(kWarning + "Connection error. " + kReset)
 
         if header.status != 200:
             print (kWarning + str(header.status) + kReset)
@@ -64,28 +63,25 @@ def getContent(testLink, delay, refresh, verbose):
 
             saleStart = datetime.datetime.strptime(start, "%m%d")
             saleEnd = datetime.datetime.strptime(end, "%m%d")
-            dateRange = getRange(saleStart, saleEnd)
+            dateRange = formatRange(saleStart, saleEnd)
 
     else:
         """
-        If lastrun.rotation is even, the last sale was posted on a Thursday so the next sale will start a day after the
-        end date of the previous sale. Otherwise, the next sale will start on the same day that the last sale ended on.
+        If lastrun.rotation is an even value, the last sale was posted on a Thursday
+        so the next sale will start a day after the end date of the previous sale.
+        Otherwise, the next sale will start on the same day that the last sale ended on.
         """
         lastSaleEnd = datetime.datetime.strptime(lastrun.lastSaleEnd, "%Y-%m-%d")
         saleStart = lastSaleEnd + datetime.timedelta((lastrun.rotation + 1) % 2)
         saleEnd = saleStart + datetime.timedelta(3) # Four-day-long sales
 
-        mdStart, mdEnd = saleStart.strftime("%m%d"), saleEnd.strftime("%m%d")
-        dmStart, dmEnd = saleStart.strftime("%d%m"), saleEnd.strftime("%d%m")
+        dateRange = formatRange(saleStart, saleEnd)
 
-        dateRange = getRange(saleStart, saleEnd)
+        monthDate = saleStart.strftime("%m%d"), saleEnd.strftime("%m%d")
+        dateMonth = saleStart.strftime("%d%m"), saleEnd.strftime("%d%m")
 
-        links = [
-            "http://na.leagueoflegends.com/en/news/store/sales/champion-and-skin-sale-{0}-{1}".format(mdStart, mdEnd),
-            "http://na.leagueoflegends.com/en/news/store/sales/champion-and-skin-sale-{0}-{1}".format(dmStart, dmEnd),
-            "http://euw.leagueoflegends.com/en/news/store/sales/champion-and-skin-sale-{0}-{1}".format(dmStart, dmEnd),
-            "http://euw.leagueoflegends.com/en/news/store/sales/champion-and-skin-sale-{0}-{1}".format(mdStart, mdEnd)
-        ]
+        links = ["http://{0}.leagueoflegends.com/en/news/store/sales/champion-and-skin-sale-{1}-{2}".format(
+            l[0], *l[1]) for l in (("na", monthDate), ("na", dateMonth), ("euw", dateMonth), ("euw", monthDate))]
 
         naLink, euwLink = None, None
 
@@ -101,7 +97,7 @@ def getContent(testLink, delay, refresh, verbose):
                 try:
                     header, content = httplib2.Http().request(link)
                 except httplib2.ServerNotFoundError:
-                    sys.exit(kWarning + "Connection error. " + kReset)
+                    sys.exit(kWarning + "Connection error." + kReset)
 
                 print link + "..." + ' ' * ("//na" in link),
 
@@ -109,7 +105,7 @@ def getContent(testLink, delay, refresh, verbose):
                     print kWarning + str(header.status) + kReset
                 else:
                     print kSuccess + "200" + kReset
-                    naLink, euwLink = links[i % 2], links[i % 2 + 2]
+                    naLink, euwLink = links[i % 2], links[(i % 2) + 2]
                     break
             else:
                 if refresh:
@@ -120,10 +116,25 @@ def getContent(testLink, delay, refresh, verbose):
 
         print kSuccess + "Post found!" + kReset + "\n"
 
-    return content, dateRange, naLink, euwLink
+    saleRegex = re.compile("<h4>(?:<a href.+?>)*\s*?(.+?)\s*?(?:<\/a>)*<\/h4>\s+?<strike.*?>(\d{3,4})<\/strike> (\d{3,4}) RP")
+    skinRegex = re.compile("(http://riot-web-static\.s3\.amazonaws\.com/images/news/Skin_Sales/\S+?\.jpg)")
+    infoRegex = re.compile("\"(http://gameinfo.na.leagueoflegends.com/en/game-info/champions/\S+?)\"")
+
+    saleArray = [Skin(), Skin(), Skin(), Champ(), Champ(), Champ()]
+
+    for i, sale in enumerate(saleArray):
+        sale.saleName, sale.regularCost, sale.saleCost = re.findall(saleRegex, content)[i]
+
+        if sale.isSkin:
+            sale.splashArt = re.findall(skinRegex, content)[i * 4]
+            sale.inGameArt = re.findall(skinRegex, content)[(i * 4) + 2]
+        else:
+            sale.infoPage = re.findall(infoRegex, content)[(i - 3) * 2]
+
+    return naLink, euwLink, dateRange, saleArray
 
 def saleOutput(sale):
-    """Generates one row of sale table"""
+    """Generates row of sale table for sale item"""
     if sale.isSkin:
         # Champions with multi-part names
         for key, value in settings.multiNames.iteritems():
@@ -139,27 +150,22 @@ def saleOutput(sale):
             else:
                 sale.champName = sale.saleName.rsplit(' ', 1)[1] # Champion name is final word of skin name
 
-        mediaString = "**[Skin Spotlight]({0})**, [Splash Art]({1}), [In-Game]({2})".format(sale.spotlight, sale.splashArt, sale.inGameArt)
+        mediaString = "**[Skin Spotlight]({0})**, [Splash Art]({1}), [In-Game]({2})".format(
+            sale.spotlight, sale.splashArt, sale.inGameArt)
     else:
-        # Generate correct splash art URL; champion name in URL is of format "Anivia", "Jarvan", "Masteryi", "Khazix"
-        sale.splashName = sale.saleName.replace('.', '').replace("'", ' ')
-        sale.splashName = "Jarvan" if sale.splashName == "Jarvan IV" else sale.splashName
-
-        try:
-            sale.splashName = sale.splashName.rsplit(' ', 1)[0] + sale.splashName.rsplit(' ', 1)[1].lower()
-        except IndexError:
-            pass
-
         sale.champName = sale.saleName
-        mediaString = "**[Champion Spotlight]({0})**, [Splash Art](http://riot-web-static.s3.amazonaws.com/images/news/Champ_Splashes/{1}_Splash.jpg)".format(sale.spotlight, sale.splashName)
+        mediaString = ("**[Champion Spotlight]({0})**, [Official Info Page]({1})".format(sale.spotlight, sale.infoPage))
 
     sale.wikiLink = "http://leagueoflegends.wikia.com/wiki/" + sale.champName.replace(" ", "_")
-    sale.icon = "[](/{0})".format(re.sub('\ |\.|\'', '', sale.champName.lower())) # Removes ' ', '.', and "'" characters from name
+
+    # Remove spaces, periods, and apostrophes from name to generate champion icon
+    sale.icon = "[](/{0})".format(re.sub('\ |\.|\'', '', sale.champName.lower()))
 
     return "|{0}|**[{1}]({2})**|{3} RP|~~{4} RP~~|{5}|".format(
         sale.icon, sale.saleName, sale.wikiLink, str(sale.saleCost), str(sale.regularCost), mediaString)
 
-def makePost(saleArray, dateRange, naLink, euwLink = "/#"):
+
+def makePost(saleArray, dateRange, naLink, euwLink):
     """Formats sale data into Reddit post"""
     rotationSchedule = [[975, 750, 520], [1350, 975, 520], [975, 750, 520], [975, 975, 520]]
     nextRotation = rotationSchedule[lastrun.rotation % 4]
@@ -197,8 +203,11 @@ def getSpotlight(name, isSkin):
         channel = "RiotGamesInc"
         suffix = "+champion+spotlight"
 
-    url = "https://www.youtube.com/user/" + channel + "/search?query=" + name.replace(" ", "+") + suffix
-    header, content = httplib2.Http().request(url)
+    try:
+        header, content = httplib2.Http().request(
+            "https://www.youtube.com/user/" + channel + "/search?query=" + name.replace(" ", "+") + suffix)
+    except httplib2.ServerNotFoundError:
+        sys.exit(kWarning + "Connection error. " + kReset)
 
     try:
         slug, spotlightName = re.findall("<h3 class=\"yt-lockup-title\"><a .* href=\"(\S*)\">(.*)<\/a><\/h3>", content)[0]
@@ -208,19 +217,8 @@ def getSpotlight(name, isSkin):
         return "https://www.youtube.com" + slug, spotlightName
 
 
-def updateLastRun():
-    """Make appropriate changes to lastrun.py if post succeeds"""
-    saleEndText = (datetime.datetime.now() + datetime.timedelta(4)).strftime("%Y-%m-%d")
-    directory = os.path.dirname(os.path.abspath(__file__))
-    path = os.path.join(directory, 'lastrun.py')
-    with open(path, 'r+') as f:
-        f.write("lastSaleEnd = \"{0}\"\nrotation = {1}\n".format(saleEndText, str((lastrun.rotation + 5) % 4)))
-
-    print kSuccess + "Updated lastrun.py." + kReset
-
-
 def submitPost(postTitle, postBody):
-    """Post to subreddits defined in settings.py"""
+    """Post to subreddits defined in settings.py and updates lastrun.py"""
     r = praw.Reddit(user_agent=settings.userAgent)
     r.login(settings.username, settings.password)
     for subreddit in settings.subreddits:
@@ -228,32 +226,24 @@ def submitPost(postTitle, postBody):
 
     print kSuccess + "Post successfully submitted to " + ", ".join(settings.subreddits) + "." + kReset
 
+    saleEndText = (datetime.datetime.now() + datetime.timedelta(4)).strftime("%Y-%m-%d")
+    directory = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(directory, 'lastrun.py')
+    with open(path, 'r+') as f:
+        f.write("lastSaleEnd = \"{0}\"\nrotation = {1}\n".format(saleEndText, str((lastrun.rotation + 5) % 4)))
 
-def parseSales(content, verbose):
-    """Finds sale data from sale page"""
-    saleRegex = re.compile("<h4>(?:<a href.+?>)*\s*?(.+?)\s*?(?:<\/a>)*<\/h4>\s+?<strike.*?>(\d{3,4})<\/strike> (\d{3,4}) RP")
-    imageRegex = re.compile("(http://riot-web-static\.s3\.amazonaws\.com/images/news/Skin_Sales/\S*?\.jpg)")
-    saleArray = [Skin(), Skin(), Skin(), Champ(), Champ(), Champ()]
-
-    for i, sale in enumerate(saleArray):
-        sale.saleName, sale.regularCost, sale.saleCost = re.findall(saleRegex, content)[i]
-
-        if sale.isSkin:
-            sale.splashArt = re.findall(imageRegex, content)[i * 4]
-            sale.inGameArt = re.findall(imageRegex, content)[(i * 4) + 2]
-
-    return saleArray
+    print kSuccess + "Updated lastrun.py." + kReset
 
 
 def manualPost():
-    """Manually enter sale data"""
+    """Manually enter sale data from the CLI"""
     saleArray = [Skin(), Skin(), Skin(), Champ(), Champ(), Champ()]
 
     inputDate = str(datetime.datetime.now().year) + click.prompt('Enter sale start date [MMDD]', type=str)
     saleStart = datetime.datetime.strptime(inputDate, "%Y%m%d")
     saleEnd = saleStart + datetime.timedelta(3)
 
-    dateRange = getRange(saleStart, saleEnd)
+    dateRange = formatRange(saleStart, saleEnd)
 
     mdStart, mdEnd = datetime.datetime.strftime(saleStart, "%m%d"), datetime.datetime.strftime(saleEnd, "%m%d")
     dmStart, dmEnd = datetime.datetime.strftime(saleStart, "%d%m"), datetime.datetime.strftime(saleEnd, "%d%m")
@@ -261,13 +251,11 @@ def manualPost():
     euwLink = "http://euw.leagueoflegends.com/en/news/store/sales/champion-and-skin-sale-{0}-{1}".format(dmStart, dmEnd)
 
     for i, sale in enumerate(saleArray):
-        print ''
-        print "Skin #{}".format(i + 1) if sale.isSkin else "Champion #{}".format(i - 2)
+        print "\nSkin #{}".format(i + 1) if sale.isSkin else "\nChampion #{}".format(i - 2)
 
         sale.saleName = str(click.prompt('Enter sale name', type=str))
-        regular = click.prompt('Enter regular cost', type=int)
-        sale.salePrice = str(math.floor(regular))
-        sale.regularPrice = str(regular)
+        sale.regularPrice = click.prompt('Enter regular cost', type=str)
+        sale.salePrice = str(math.floor(int(sale.regularPrice)))
 
         if sale.isSkin:
             sale.splashArt = str(click.prompt('Splash art URL', type=str))
@@ -278,7 +266,7 @@ def manualPost():
 
 @click.command()
 @click.argument('testLink', required=False)
-@click.option('--delay', '-d', default=0, metavar='<hours>', help='Delay before running script.')
+@click.option('--delay', '-d', 'delay', default=0, metavar='<hours>', help='Delay before running script.')
 @click.option('--manual', '-m', 'manual', is_flag=True, help='Manually create post.')
 @click.option('--refresh', '-r', 'refresh', is_flag=True, help='Automatically refresh sale pages.')
 @click.option('--verbose', '-v', 'verbose', is_flag=True, help='Output entire post body (for piping to pbcopy).')
@@ -289,34 +277,30 @@ def main(testLink, delay, manual, refresh, verbose):
     Legends champion and skin sales. Uses the httplib2 and PRAW libraries.
     """
 
-    if manual:
-        naLink, euwLink, dateRange, saleArray = manualPost()
-    else:
-        content, dateRange, naLink, euwLink = getContent(testLink, delay, refresh, verbose)
-        saleArray = parseSales(content, verbose)
+    naLink, euwLink, dateRange, saleArray = manualPost() if manual else getContent(testLink, delay, refresh, verbose)
 
-    # Post title
+    # Generate post title
     postTitle = "[Skin Sale] " + ", ".join(sale.saleName for sale in saleArray if sale.isSkin) + " (" + dateRange + ")"
+
     if not verbose:
         print kSpecial + postTitle + kReset
 
-    # Get spotlights
+    # Get spotlights and print to terminal
     for sale in saleArray:
         sale.spotlight, sale.spotlightName = getSpotlight(sale.saleName, sale.isSkin)
         if not verbose:
-            print '\t'.join('{: <24}'.format(s) for s in [sale.saleName, sale.saleCost + " RP", sale.spotlightName])
+            print '{: <30}'.format(sale.saleName) + sale.saleCost + " RP\t" + sale.spotlightName
 
-    # Post body
     postBody = makePost(saleArray, dateRange, naLink, euwLink)
+
     if verbose:
         print postBody
 
-    if manual and (not click.confirm('Do you want to continue?')):
-        sys.exit(kWarning + "Did not post." + kReset)
-
     if not testLink:
-        submitPost(postTitle, postBody)
-        updateLastRun()
+        if manual and not click.confirm('Post to Reddit?'):
+            sys.exit(kWarning + "Did not post." + kReset)
+        else:
+            submitPost(postTitle, postBody)
 
     sys.exit(0)
 
